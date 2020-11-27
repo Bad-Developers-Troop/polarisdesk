8e3add9528773e117f038531fc474451af17c9d0
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2020-11-27 14:40:22 +0100

    Dev (#12)
    * add gitignore
    
    * first commit with identity server
    
    * Change launchSettings.json to work without IIS (ex.linux) (#5)
    
    * Add PlantUML Schema (#11)
    
    * Change launchSettings.json to work without IIS (ex.linux)
    
    * Add PlantUML Schema
    
    Co-authored-by: Francesco Danti <fdanti@gmail.com>

---
+++ src/PolarisDesk.IdentityProvider/Config.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace PolarisDesk.IdentityProvider
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+                   new IdentityResource[]
+                   {
+                        new IdentityResources.OpenId(),
+                        new IdentityResources.Profile(),
+                        new IdentityResources.Email()
+                   };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("polarisdeskapi","PolarisDesk API")
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "polarisdeskapp",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+                    AllowedCorsOrigins = { "https://localhost:44396" },
+                    AllowedScopes = { "openid", "profile", "email" , "polarisdeskapi" },
+                    RedirectUris = { "https://localhost:44396/authentication/login-callback" },
+                    PostLogoutRedirectUris = { "https://localhost:44396/" },
+                    Enabled = true
+                }
+            };
+    }
+}
---
+++ src/PolarisDesk.Web/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace PolarisDesk.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                builder.Configuration.Bind("oidc", options.ProviderOptions);
+            });
+
+            builder.Services.AddHttpClient("api")
+                .AddHttpMessageHandler(sp =>
+                {
+                    var handler = sp.GetService<AuthorizationMessageHandler>()
+                        .ConfigureHandler(
+                            authorizedUrls: new[] { "https://localhost:44312" },
+                            scopes: new[] { "polarisdeskapi" });
+                    return handler;
+                });
+
+            builder.Services.AddScoped(
+                sp => sp.GetService<IHttpClientFactory>().CreateClient("api"));
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
