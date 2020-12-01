ee7d10df249ce213f82d3fd97d5cde6ce596c1ff
Andrea Merlin <a.merlin@keantex.com>
2020-12-02 00:41:52 +0100

    ICrudService added (#15)
    * ICrudService added
    TicketService added
    
    * fix code

---
+++ src/PolarisDesk.API/Services/TicketService.cs
@@ -0,0 +1,35 @@
+﻿
+using PolarisDesk.API.Interface;
+using System.Threading.Tasks;
+
+namespace PolarisDesk.API.Services
+{
+
+    public class TicketService<T, TKey> : ICrudService<T, TKey> where T: class
+    {
+        public Task Create(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(TKey id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<T> Get(TKey id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<T[]> GetList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Update(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
