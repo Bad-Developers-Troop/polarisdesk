c84077aab5a4045b50a405533c7b7e1f80bfd03c
Andrea Merlin <a.merlin@keantex.com>
2020-12-10 23:10:40 +0100

    mock customer api service

--- src/PolarisDesk.API/Startup.cs
+++ src/PolarisDesk.API/Startup.cs
@@ -38,9 +38,9 @@ namespace PolarisDesk.API
             //Dep
 #if DEBUG            
             services.AddTransient<ICrudService<Ticket,Guid>, TicketServiceMock>();
+            services.AddTransient<ICrudService<Customer, Guid>, CustomerServiceMock>();
             services.AddTransient<ICrudService<TicketStatus, Guid>, TicketStatusService<TicketStatus, Guid>>();
             services.AddTransient<ICrudService<TicketPriority, Guid>, TicketPriorityService<TicketPriority, Guid>>();
-            services.AddTransient<ICrudService<Customer, Guid>, CustomerService<Customer, Guid>>();
 #endif
 
 #if !DEBUG
